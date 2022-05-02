dbfc8cf8df4d6e94e50ece4e3204e4b940d5e2a5
Veronika Pilipenko <veronika.pilipenko@jaanhealth.com>
2022-05-02 18:51:55 -0400

    lol

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import React from "react";
+import { useState } from "react";
 import Vent from "./components/vent";
 import Blanket from "./components/blanket";
-
+import './stylesheets/main.css'
 
 function App() {
+
+  const [blanket, setBlanket] = useState();
+  const [vent, setVent] = useState();
+
+  const handleVent = () => {
+    setVent();
+  }
+
+  const handleBlanket = () => {
+    setBlanket();
+  }
+
   return (
     <div className="App">
-      <div>
-        <Blanket/>
-        <Vent/>
+      <div className="outer-container">
+        <h1>How would you like to proceed?</h1>
+        <h2>Answer a series of questions to figure out a little bit more about yourself</h2>
+        <h3>If you were an inanimate object, which one would best describe you?</h3>
+          <select>
+            <option onClick={handleVent}>{vent}</option>
+            <option onClick={handleBlanket}>{blanket}</option>
+          </select>
       </div>
     </div>
   );
